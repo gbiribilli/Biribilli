d0472595df76dcd4d0e7f18d19038fa81deaee3b
gbiribilli <110321523+gbiribilli@users.noreply.github.com>
2024-02-20 13:46:24 -0300

    Add files via upload
    Tentativas e erros dos desafios do Curso

---
+++ Desafios/app.js
@@ -0,0 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function verificar(){
+    console.log('O botão ')
+}
+
+function alertar(){
+    alert('Eu amo JS');
+}
+
+function promp(){
+   let cidade = prompt('Diga uma cidade do Brasil');
+   alert(`Estive em ${nomeDaCidade} e lembrei de você`);
+}
+
+function soma(){
+    ad = window.prompt("Me de um numero");
+    aicao = window.prompt('Me de outro numero');
+    total = ad + aicao;
+   
+    window.alert('O resultado é ' + total);
+}
